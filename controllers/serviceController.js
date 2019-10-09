7d14645a009fcc67e3ac4580792f81f34b4b334f
Tom Doughty <thomashdoughty@gmail.com>
2019-10-09 09:22:14 +0100

    Refactor to use async await

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -15,46 +15,24 @@ exports.search = ({ params }, res) => {
 	return res.render('search.html', { service });
 };
 
-exports.results = ({ params, body: { latitude, longitude, location} }, res) => {
-	// Get service type constant
+exports.results = async({ params, body: { latitude, longitude, location } }, res) => {
 	const service = SERVICE_TYPES[params.service.toUpperCase()];
-	
-	// Default object to be passed to the view
-	const renderObj = {
-		// Service type object
-		service,
-		// Postcode or geolocation default
-		location: location ? `"${location.toUpperCase()}"` : 'your location'
-	};
-	
-	// If coordinates are in the body skip postcode lookup
-	if (latitude && longitude) {
-		// Get services
-		getServices(latitude, longitude, service.code)
-			// Render results with services
-			.then(services => res.render('results.html', {
-				...renderObj,
-				services
-			}));
-	} else {
-		// Get coordinated from postcode
-		getLatLong(location)
-			.then(({ latitude, longitude }) => {
-				// Get services
-				getServices(latitude, longitude, service.code).then(services => {
-					// Render results with services
-					return res.render('results.html', {
-						...renderObj,
-						services
-					})
-				});
-		})
-		.catch(() => {
-			// Show validation error for invalid postcode
-			return res.render('search.html', { 
-				errorMessage: 'Please enter a valid postcode',
-				service
-			})
+
+	try {
+		if (!latitude || !longitude) {
+			({ latitude, longitude } = await getLatLong(location));
+		}
+		return res.render('results.html', {
+			service,
+			services: await getServices(latitude, longitude, service.code),
+			location: location ? `"${location.toUpperCase()}"` : 'your location'
 		});
+
+	} catch(err) {
+		console.log(err)
+		return res.render('search.html', { 
+			errorMessage: 'Please enter a valid postcode',
+			service
+		})
 	}
 };
