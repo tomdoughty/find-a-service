1364dce1c1fd9c9407855a8000478019f4ec3ff8
Tom Doughty <thomashdoughty@gmail.com>
2019-11-02 19:25:15 +0000

    Break tests

--- utils/getService.js
+++ utils/getService.js
@@ -1,6 +1,7 @@
 const SERVICE_TYPES = require('../constants');
 
 module.exports = (req) => {
-  if (!req || req.service || req.params.service) return null;
-  return SERVICE_TYPES[req.params.service.toUpperCase()] || null;
+  if (!req || req.service) return null;
+  const res = SERVICE_TYPES[req.params.service.toUpperCase()] || null;
+  return res;
 };
--- utils/getService.test.js
+++ utils/getService.test.js
@@ -1,9 +1,28 @@
 const getService = require('../utils/getService');
 
 describe('getService util', () => {
-  describe('return null', () => {
+  describe('returns null', () => {
     it('if the key does not exist', () => {
-      expect(getService({ params: { service: 'Tom' } })).toBeNull();
+      expect(getService({ params: { service: 'Tom' } }))
+        .toBeNull();
+    });
+  });
+  describe('results service object', () => {
+    it('if the key exists', () => {
+      expect(getService({ params: { service: 'DENTIST' } }))
+        .toEqual({
+          key: 'DENTIST',
+          code: 'DEN',
+          display: 'Dentist',
+        });
+    });
+    it('if the key exists in lowercase', () => {
+      expect(getService({ params: { service: 'dentist' } }))
+        .toEqual({
+          key: 'GP',
+          code: 'DEN',
+          display: 'Dentist',
+        });
     });
   });
 });
