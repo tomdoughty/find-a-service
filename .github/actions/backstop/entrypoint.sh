c86fd80b931f0b3870a303b2d0abc5c080be9f6b
Tom Doughty <thomashdoughty@gmail.com>
2019-11-04 18:46:57 +0000

    Update permissions

--- .github/actions/backstop/entrypoint.sh
+++ .github/actions/backstop/entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-chmod + x /docker-entrypoint.sh
 
 # Exit if any subcommand fails
 set -e 
 
-setup="NODE_ENV=development npm install &&"
+# Setup node modules if needed
+if [ -e node_modules/.bin/jest ]; then
+    setup=""
+else
+    echo "## Your environment is not ready yet. Installing modules..."
+    if [ -f yarn.lock ]; then
+        setup="yarn --non-interactive --silent --ignore-scripts --production=false &&"
+    else
+        setup="NODE_ENV=development npm install &&"
+    fi
+fi
+
 
 echo "## Running Jest"
 args=$@
